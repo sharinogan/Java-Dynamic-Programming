a5c1108c9120496709aff7c3ce1411d1a144f0db
Voragun Supawong <voragun2543@gmail.com>
2022-08-29 21:24:06 +0700

    [not finish] Palindrome Partitioning

--- code/Start.java
+++ code/Start.java
@@ -1,48 +1,56 @@
 class Start {
     
     public static void main(String[] data) {
-        int[] cuts = { 1, 3, 4, 5 };
-        int wood_size = 7;
+        String test = "eegiicgaeadbcfacfhifdbiehbgejcaeggcgbahfcajfhjjdgj";
         
-        Wood w = new Wood();
-        System.out.println(w.minCost(wood_size, cuts));
+        Solution s = new Solution();
+        int r = s.minCut(test);
+        System.out.println(r);
     }
     
 }
 
-class Wood {
+class Solution {
     
-    int[][] matrix;
-    public int minCost(int n, int[] cuts) {        
-       // first, we have to sort  
-        java.util.Arrays.sort(cuts);
-        int[] points = new int[cuts.length + 2];
-        points[0] = 0; points[points.length - 1] = n;
-        for(int i = 0; i < cuts.length; i ++)
-            points[i+1] = cuts[i];
-        matrix = new int[points.length][points.length];
-        for(int i = 0; i < matrix.length; i++)
-            for (int j = 0; j < matrix.length; j++)
-                matrix[i][j] = Integer.MAX_VALUE;
-        return go(points, 0, points.length - 1);
+    String test = "eegiicgaeadbcfacfhifdbiehbgejcaeggcgbahfcajfhjjdgj";
+    
+    public int minCut(String s) {
+        char[] a = s.toCharArray();
+        int[][] matrix = new int[a.length][a.length];
+        for (int width = 0; width < a.length; width++) {
+            for (int start = 0; start + width < a.length; start++) {
+                int finish = start + width;
+                if (start == width) { 
+                    matrix[start][finish] = 0; 
+                    continue;
+                }
+                if (check(a, start, finish)) {
+                    matrix[start][finish] = 0;
+                    continue;
+                }
+                int best = Integer.MAX_VALUE;
+                for (int i = start; i < finish; i++) {
+                    int total = matrix[start][i] + 1 + matrix[i+1][finish];
+                    if (total < best) best = total;
+                }
+                if (best == Integer.MAX_VALUE) best = 0;
+                matrix[start][finish] = best;
+            }
+        }
+        return matrix[0][a.length - 1];
     }
     
-    int go(int[] points, int start, int finish) {
-       if (start >= finish) return Integer.MAX_VALUE;
-        if (matrix[start][finish] != Integer.MAX_VALUE)
-            return matrix[start][finish]; // use previous value
-        
-        int best = Integer.MAX_VALUE;  // about 2 billion
-        // try to cuts every point from (start, finish)
-        for(int i = start + 1; i < finish; i++) {
-            int cost = points[finish] - points[start]; // cost of cutting
-            int left = go(points, start, i);
-            int right = go(points, i, finish);
-            int total = cost + left + right;
-            if (total < best) best = total;
+    
+    boolean check(char[] a, int head, int tail) {
+        int left = head;
+        int right = tail;
+        while(left < right) {
+            if(a[left] != a[right]) { 
+                return false; 
+            } 
+            left++;
+            right--;
         }
-        if (best == Integer.MAX_VALUE) best = 0;
-        matrix[start][finish] = best;
-        return best;
+        return true;
     }
 }
